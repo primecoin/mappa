787c0deef418b8e927fd3f791b4f35d90e848a02
Sunny King <sunnyking9999@gmail.com>
2019-05-27 17:32:28 +0000

    Wrapper API for searchrawtransactions and getaddressbalance

---
+++ app.py
@@ -0,0 +1,42 @@
+# app.py
+
+from flask import Flask, jsonify
+
+import json
+import requests
+
+app = Flask(__name__)
+
+# Node URL for JSON-RPC queries
+nodeURL = "http://xpm:ZGkadYaDMKVyIQcVVw8D87njCJntSmRddM3FdmVcI6A=@rpc16.primecoin.org:9912"
+
+@app.route('/api/rpc/searchrawtransactions/<address>')
+def searchRawTransactions(address):
+    headers = {'content-type': 'application/json'}
+    payload = {
+        "method": "searchrawtransactions",
+        "params": [address],
+        "jsonrpc": "1.0",
+        "id": 0,
+    }
+    response = requests.post(nodeURL, data=json.dumps(payload), headers=headers).json()
+    return jsonify(response), 200
+
+
+@app.route('/api/rpc/getaddressbalance/<address>')
+def getAddressBalance(address):
+    headers = {'content-type': 'application/json'}
+    payload = {
+        "method": "getaddressbalance",
+        "params": [ json.dumps({"addresses": [address]}) ],
+        "jsonrpc": "1.0",
+        "id": 0,
+    }
+    response = requests.post(nodeURL, data=json.dumps(payload), headers=headers).json()
+    return jsonify(response), 200
+
+
+# include this for local dev
+
+if __name__ == '__main__':
+    app.run()
