8b572b9439a32319f3b8f6830a3cc3c49af52a2f
Sunny King <sunnyking9999@gmail.com>
2019-05-28 21:53:13 +0000

    * add api for getblockbyheight, getblockbyhash, getrawtransaction * use verbose mode for block related api's to show tx detail

--- app.py
+++ app.py
@@ -10,53 +10,61 @@ app = Flask(__name__)
 # Node URL for JSON-RPC queries
 nodeURL = "http://xpm:ZGkadYaDMKVyIQcVVw8D87njCJntSmRddM3FdmVcI6A=@rpc16.primecoin.org:9912"
 
-@app.route('/api/rpc/searchrawtransactions/<address>')
-def searchRawTransactions(address):
+def requestJsonRPC(method, params):
     headers = {'content-type': 'application/json'}
     payload = {
-        "method": "searchrawtransactions",
-        "params": [address],
+        "method": method,
+        "params": params,
         "jsonrpc": "1.0",
         "id": "zappa-explorer",
     }
-    response = requests.post(nodeURL, data=json.dumps(payload), headers=headers).json()
-    return jsonify(response), 200
+    return requests.post(nodeURL, data=json.dumps(payload), headers=headers).json()
 
+
+@app.route('/api/rpc/searchrawtransactions/<address>')
+def searchRawTransactions(address):
+    response = requestJsonRPC("searchrawtransactions", [ address])
+    return jsonify(response), 200
 
 @app.route('/api/rpc/getaddressbalance/<address>')
 def getAddressBalance(address):
-    headers = {'content-type': 'application/json'}
-    payload = {
-        "method": "getaddressbalance",
-        "params": [ json.dumps({"addresses": [address]}) ],
-        "jsonrpc": "1.0",
-        "id": "zappa-explorer",
-    }
-    response = requests.post(nodeURL, data=json.dumps(payload), headers=headers).json()
+    response = requestJsonRPC("getaddressbalance", [ json.dumps({"addresses": [address]}) ] )
     return jsonify(response), 200
-
 
 @app.route('/api/rpc/getbestblock/')
 def getBestBlock():
-    headers = {'content-type': 'application/json'}
-    payload = {
-        "method": "getbestblockhash",
-        "params": [],
-        "jsonrpc": "1.0",
-        "id": "zappa-explorer",
-    }
-    blockHash = requests.post(nodeURL, data=json.dumps(payload), headers=headers).json()["result"]
-    payload = {
-        "method": "getblock",
-        "params": [ blockHash ],
-        "jsonrpc": "1.0",
-        "id": "zappa-explorer",
-    }
-    response = requests.post(nodeURL, data=json.dumps(payload), headers=headers).json()
+    response = requestJsonRPC("getbestblockhash", [])
+    if "error" in response and response["error"] != None:
+        return jsonify(response), 200
+    else:
+        blockHash = response["result"]
+        response = requestJsonRPC("getblock", [blockHash, 2])
+        return jsonify(response), 200
+
+@app.route('/api/rpc/getblockbyheight/<height>')
+def getBlockByHeight(height):
+    try:
+        heightInteger = int(height)
+    except:
+        heightInteger = height
+    response = requestJsonRPC("getblockhash", [ heightInteger ])
+    if "error" in response and response["error"] != None:
+        return jsonify(response), 200
+    else:
+        blockHash = response["result"]
+        response = requestJsonRPC("getblock", [blockHash, 2])
+        return jsonify(response), 200
+
+@app.route('/api/rpc/getblockbyhash/<blockHash>')
+def getBlockByHash(blockHash):
+    response = requestJsonRPC("getblock", [ blockHash, 2])
     return jsonify(response), 200
 
+@app.route('/api/rpc/getrawtransaction/<txid>')
+def getRawTransaction(txid):
+    response = requestJsonRPC("getrawtransaction", [txid, True])
+    return jsonify(response), 200
 
 # include this for local dev
-
 if __name__ == '__main__':
     app.run()
