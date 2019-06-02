f577a5169a26e89bb1cbd7192c20aa8bd13f0523
Sunny King <sunnyking9999@gmail.com>
2019-06-02 22:59:51 +0000

    Add runtime configuration check for proper configurations

--- README.md
+++ README.md
@@ -15,9 +15,20 @@ NETWORK = "mainnet"
 
 ```
 # instance/config.py
+#
+# To open node rpc to the Internet, enable rpc settings in node configuration.
+#
+# For Satoshi 0.8 nodes (Primecoin 0.1.2 production):
+# rpcuser=<rpc user name>
+# rpcpassword=<rpc password>
+# rpcallowip=*
+#
+# For Satoshi 0.16 nodes (Primecoin development)
+# rpcauth=<output line from share/rpcauth/rpcauth.py>
+# rpcallowip=0.0.0.0/0
 
-MAINNET_RPC_URL = "<mainnet url>"
-TESTNET_RPC_URL = "<testnet url>"
+MAINNET_RPC_URL = "http://<rpcuser>:<rpcpassword>@<nodedomain>:9912"
+TESTNET_RPC_URL = "http://<rpcuser>:<rpcpassword>@<nodedomain>:9914"
 ```
 
 Test locally:
--- app.py
+++ app.py
@@ -8,6 +8,14 @@ import requests
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
+if 'NETWORK' not in app.config:
+    raise RuntimeError("Setting 'NETWORK' is not configured")
+if app.config['NETWORK'] != 'mainnet' and app.config['NETWORK'] != 'testnet':
+    raise RuntimeError("Setting 'NETWORK' can only be 'mainnet' or 'testnet'")
+if app.config['NETWORK'] == 'mainnet' and 'MAINNET_RPC_URL' not in app.config:
+    raise RuntimeError("Setting 'MAINNET_RPC_URL' is not configured")
+if app.config['NETWORK'] == 'testnet' and 'TESTNET_RPC_URL' not in app.config:
+    raise RuntimeError("Setting 'TESTNET_RPC_URL' is not configured")
 
 
 def requestJsonRPC(method, params):
@@ -18,7 +26,7 @@ def requestJsonRPC(method, params):
         "jsonrpc": "1.0",
         "id": "zappa-explorer",
     }
-    url = app.config['MAINNET_RPC_URL'] if app.config['NETWORK'] != 'testnet' else app.config['TESTNET_RPC_URL']
+    url = app.config['MAINNET_RPC_URL'] if app.config['NETWORK'] == 'mainnet' else app.config['TESTNET_RPC_URL']
     return requests.post(url, data=json.dumps(payload), headers=headers).json()
 
 
